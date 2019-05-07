4e51b38638f038f59e3dccc45d690eaa9e3c0508
Thopic <thomas.dupic@lpt.ens.fr>
2019-05-07 12:02:23 +0200

    Order

--- chordialement/core.py
+++ chordialement/core.py
@@ -9,17 +9,6 @@ import numpy as np
 import pandas as pd
 
 
-def get_idx_interv(d, D):
-    k = 0
-    while(d > D[k]):
-        k += 1
-    return k-1
-
-
-def dist(A, B):
-    return np.linalg.norm(np.array(A) - np.array(B))
-
-
 def deCasteljau(b, t):
     N = len(b)
     a = np.copy(b)
@@ -60,30 +49,28 @@ def linear_gradient_color(c1, c2, n=10):
 
 
 def draw_chord(A, B, ax=None, color_start="b", color_end="r",
-               bezier_prms=([0, 0.765, 1.414, 1.848, 2], [1.2, 1.5, 1.8, 2.]),
-               precision=1000,
-               **kwargs):
+               precision=1000, **kwargs):
     """ Draw a Bezier curve between two points """
-    d = dist(A, B)
+    d = np.linalg.norm(np.array(A) - np.array(B))
 
     # Depending on the distance between the points
     # the Bezier curve parameters change
-    K = get_idx_interv(d, bezier_prms[0])
-    b = [A, A/bezier_prms[1][K], B/bezier_prms[1][K], B]
-    bz = BezierCv(b, nr=100)
+    b = [A, A/(1 + d), B/(1 + d), B]
+    bz = BezierCv(b, nr=precision)
 
     lc = mcoll.LineCollection(bz,
                               colors=linear_gradient_color(color_start,
                                                            color_end,
-                                                           n=100), **kwargs)
+                                                           n=precision), **kwargs)
     ax.add_collection(lc)
 
 
-def draw_arc_circle(start, end, color="b", ax=None,
+def draw_arc_circle(start, end, color="b", radius=1, ax=None,
                     thickness=0.1, precision=1000):
     ts = np.linspace(start, end, precision)
-    poly_nodes = ([position_circle(t) for t in ts] +
-                  [position_circle(t, radius=1+thickness) for t in ts[::-1]])
+    poly_nodes = ([position_circle(t, radius=radius) for t in ts] +
+                  [position_circle(t, radius=radius+thickness)
+                   for t in ts[::-1]])
     x, y = zip(*poly_nodes)
     ax.fill(x, y, color=color)
 
@@ -93,10 +80,54 @@ def add_text_circle(x, txt, radius=1, ax=None, **kwargs):
             txt, rotation=360*x, ha='center', va='center', **kwargs)
 
 
+def order_data(nodes, links, order=None):
+    """
+    Return a correctly ordered dataframe, ready to be plotted in chord format
+    @ Args:
+    - nodes: a dictionary that associates to each unique nodes a category name
+    - links: link nodes together
+    - order: order of the categories
+    """
+    categories = list(set(nodes.values()))
+    ndsnb = dict(zip(nodes.keys(), range(len(nodes))))
+    
+    # decide on the order
+    if(order is None):
+        order = sorted(categories)
+    df = pd.Series(nodes).to_frame("categorie")
+    df["nbcat"] = df.categorie.map(dict(zip(order, range(len(order)))))
+
+    df = df.rename_axis('source').reset_index()
+    df["nbsource"] = df.source.map(ndsnb)
+    
+    dict_links = {}
+    for l1, l2 in links:
+        dict_links[l1] = l2
+        dict_links[l2] = l1
+        
+    df["target"] = df.source.map(dict_links)
+    df["nbtarget"] = df.target.map(ndsnb)
+    df["tgt_nbcat"] = df.target.map(df.set_index("source").nbcat)
+
+    df["tgt_cat_order"] = df.apply(
+        lambda r: (len(order)-1+r["nbcat"]-r["tgt_nbcat"]) % (len(order)-1),
+        axis=1)
+    df["sort_order"] = df.apply(
+        lambda r: (r["nbsource"] if r["nbcat"] <= r["tgt_nbcat"]
+                   else -r["nbtarget"]),
+        axis=1)
+    df = df.sort_values(by=["nbcat", "tgt_cat_order", "sort_order"])
+    return df
+   
+    
 def chord_diagram(source, target, hue="black", data=None, ax=None,
-           palette=sns.color_palette(), categorie_internal_chords=False, 
-                  precision_chord=100, precision_circle=100, thickness_circle=0.1, no_chords=False,
-                  radius_text=1.1, circle_args={}, text_args={}, chord_args={}):
+                  hue_order=None, palette=sns.color_palette(),
+                  categorie_internal_chords=False, sub_circle=False,
+                  spacing=0, spacing_sub=0, inverted=False,
+                  precision_chord=100, precision_circle=100,
+                  thickness_circle=0.1, thickness_sub_circle=0.05,
+                  radius_text=1.1, no_chords=False, circle_args={},
+                  text_args={}, chord_args={}):
     """ Draw a chord diagram
     @ Arguments:
     - source: A unique index for each of the chords. The order
@@ -108,6 +139,7 @@ def chord_diagram(source, target, hue="black", data=None, ax=None,
     - hue: Can be either a color name, a column name (if data is not
     None), or an array. In the two last case, the color are given 
     by palette in order.
+    - hue_order: The order in which the hue should be drawn
     - data: None or a dataframe containing columns source and target
     - ax: matplotlib.ax object in which to draw. If None, a figure 
     is created.
@@ -130,60 +162,74 @@ def chord_diagram(source, target, hue="black", data=None, ax=None,
     draws the chords of the diagram.
     """
     if data is not None:
-        source = data[source]
-        target = data[target]
+        source = data[source].values
+        target = data[target].values
         if hue in data.keys():
-            hue = data[hue]
+            hue = data[hue].values
 
-    # remove duplicate, conserve order
-    order = list(dict.fromkeys(hue))
-            
-    df = pd.DataFrame({"idx": source, "target": target, "categorie": hue})
-    df["cat_nb"] = df.categorie.map(dict(zip(order, range(len(order)))))
+    if hue_order is None:
+        hue_order = list(dict.fromkeys(hue))    
+    nodes = dict(zip(source, hue))
+
+    links = list(zip(source, target))
+    df = order_data(nodes, links, hue_order)
     
-    df_unique_idx = df.groupby("idx", sort=False).agg({'cat_nb':'first', 'categorie':'first'})
-    df_unique_idx["position"] = np.linspace(0, 1, len(df_unique_idx))
-    df["position"] = df.idx.map(df_unique_idx.position)
-    df["tgt_position"] = df.target.map(df.set_index("idx").position.to_dict())
-    df["tgt_cat_nb"] = df.target.map(df.set_index("idx").cat_nb.to_dict()).apply(lambda x: int(x))
-    
-    nb_to_name_cat = dict(zip(range(len(order)), order))
-    
-    if(len(palette) < len(order)):
-        warnings.warn("Not enough colors in the palette, switching "
-                      "to Seaborn husl palette.")
+    idxs = list(np.where(df.nbcat.values[:-1] != df.nbcat.values[1:])[0])
+    x = 0
+    positions = []
+    for i in range(len(df)):
+        positions.append(x)
+        if i in idxs:
+            x += spacing
+        x += (1 - spacing*(len(idxs)+1))/(len(df))
+    df["position"] = positions
+    df["tgt_position"] = df.target.map(df.set_index("source").position)
+        
+    if(len(palette) < len(hue_order)):
+        warnings.warn("Not enough colors in the palette ({} needed), switching "
+                      "to Seaborn husl palette.".format(len(hue_order)))
         palette = sns.color_palette("husl", len(order))
 
     if(ax is None):
         fig, ax = plt.subplots()
 
-    positions = df_unique_idx.position.values
-    previous_cat = df_unique_idx.cat_nb.values[0]
-    start_categorie = [positions[0]]
-    end_categorie = []
-    categorie = [previous_cat]
-    for i, cat in enumerate(df_unique_idx.cat_nb.values):
-        if cat != previous_cat:
-            print(i)
-            start_categorie.append(positions[i])
-            end_categorie.append(positions[i-1])
-            categorie.append(cat)
-            previous_cat = cat
-    end_categorie.append(positions[-1])
-    for s, e, c in zip(start_categorie, end_categorie, categorie):
-        print(s, e, c, nb_to_name_cat[c])
-        draw_arc_circle(s, e, color=palette[c], ax=ax, precision=precision_circle, thickness=thickness_circle, **circle_args)
-        add_text_circle((s + e)/2, nb_to_name_cat[c], ax=ax, color=palette[c], radius=radius_text, **text_args)
+    nb_to_name_cat = dict(enumerate(hue_order))
+    positions = df.position.values
+    tgt_cat = df.nbcat.values
+    idxs = np.where(tgt_cat[:-1] != tgt_cat[1:])[0]
+    start_categorie = [0] + list(positions[idxs+1])
+    end_categorie = list(positions[idxs]) + [positions[-1]]
+    cats = [tgt_cat[0]] + list(tgt_cat[idxs+1])
+    
+    for s, e, c in zip(start_categorie, end_categorie, cats):
+        draw_arc_circle(s - 0.5/len(df), e + 0.5/len(df), color=palette[c], ax=ax,
+                        precision=precision_circle, thickness=thickness_circle,
+                        radius=(1+thickness_sub_circle + spacing_sub*2*math.pi if inverted else 1),**circle_args)
+        add_text_circle((s + e - 1/len(df))/2, nb_to_name_cat[c], ax=ax, color=palette[c], radius=radius_text, **text_args)
 
+    if sub_circle:
+        df["both_cat"] = df.apply(lambda r: str(r["nbcat"]) + "_" + str(r["tgt_nbcat"]), axis=1)
+        positions = df.position.values
+        tgt_cat = df.both_cat.values
+        idxs = np.where(tgt_cat[:-1] != tgt_cat[1:])[0]
+        start_categorie = [0] + list(positions[idxs+1])
+        end_categorie = list(positions[idxs]) + [positions[-1]]
+        subcat = [df.tgt_nbcat.values[0]] + list(df.tgt_nbcat.values[idxs+1])
+        for s, e, c in zip(start_categorie, end_categorie, subcat):
+            draw_arc_circle(s-0.5/len(df), e + 0.5/len(df), color=palette[c], ax=ax, precision=precision_circle,
+                            thickness=thickness_sub_circle,
+                            radius=(1 if inverted else 1+thickness_circle+spacing_sub*2*math.pi), **circle_args)
+
+        
     for jj, src_p, tgt_p, src_c, tgt_c in zip(range(len(df)), df["position"].values, 
                                           df["tgt_position"].values,
-                                          df["cat_nb"].values, 
-                                          df["tgt_cat_nb"].values):
-        if not (no_chords and (src_p == tgt_p or (not categorie_internal_chords and src_c == tgt_c))):
+                                          df["nbcat"].values, 
+                                          df["tgt_nbcat"].values):
+        if not (no_chords or (src_p == tgt_p or (not categorie_internal_chords and src_c == tgt_c))):
             draw_chord(position_circle(src_p),
                         position_circle(tgt_p), ax=ax,
-                        color_start=palette[src_c],
-                        color_end=palette[tgt_c],
+                        color_start=palette[(tgt_c if inverted else src_c)],
+                        color_end=palette[(src_c if inverted else tgt_c)],
                         precision=precision_chord,
                         **chord_args)
 
